a09b0e0ea03db40e772f48d831c1d0308c739926
Christian Haettich <feddischson@gmx.com>
2017-01-23 21:40:56 +0100

    removes obsolte spaces within the template files

--- inc/_template_.h
+++ inc/_template_.h
@@ -2,21 +2,21 @@
 //
 // Copyright (C) 2017  Christian Haettich [feddischson]
 //
-// This program is free software; you can redistribute it 
-// and/or modify it under the terms of the GNU General Public 
-// License as published by the Free Software Foundation; 
-// either version 3 of the License, or (at your option) 
+// This program is free software; you can redistribute it
+// and/or modify it under the terms of the GNU General Public
+// License as published by the Free Software Foundation;
+// either version 3 of the License, or (at your option)
 // any later version.
 //
-// This program is distributed in the hope that it will 
-// be useful, but WITHOUT ANY WARRANTY; without even the 
-// implied warranty of MERCHANTABILITY or FITNESS FOR A 
-// PARTICULAR PURPOSE. See the GNU General Public License 
+// This program is distributed in the hope that it will
+// be useful, but WITHOUT ANY WARRANTY; without even the
+// implied warranty of MERCHANTABILITY or FITNESS FOR A
+// PARTICULAR PURPOSE. See the GNU General Public License
 // for more details.
 //
-// You should have received a copy of the GNU General 
-// Public License along with this program; if not, see 
-// <http://www.gnu.org/licenses/>. 
+// You should have received a copy of the GNU General
+// Public License along with this program; if not, see
+// <http://www.gnu.org/licenses/>.
 //
 #ifndef TEMPLATE_H
 #define TEMPLATE_H
@@ -74,7 +74,7 @@ private:
 
 }; // namespace INCLUDE_GARDENER
 
-#endif // TEMPLATE_H 
+#endif // TEMPLATE_H
 
 // vim: filetype=cpp et ts=3 sw=3 sts=3
 
--- src/_template_.cpp
+++ src/_template_.cpp
@@ -2,21 +2,21 @@
 //
 // Copyright (C) 2017  Christian Haettich [feddischson]
 //
-// This program is free software; you can redistribute it 
-// and/or modify it under the terms of the GNU General Public 
-// License as published by the Free Software Foundation; 
-// either version 3 of the License, or (at your option) 
+// This program is free software; you can redistribute it
+// and/or modify it under the terms of the GNU General Public
+// License as published by the Free Software Foundation;
+// either version 3 of the License, or (at your option)
 // any later version.
 //
-// This program is distributed in the hope that it will 
-// be useful, but WITHOUT ANY WARRANTY; without even the 
-// implied warranty of MERCHANTABILITY or FITNESS FOR A 
-// PARTICULAR PURPOSE. See the GNU General Public License 
+// This program is distributed in the hope that it will
+// be useful, but WITHOUT ANY WARRANTY; without even the
+// implied warranty of MERCHANTABILITY or FITNESS FOR A
+// PARTICULAR PURPOSE. See the GNU General Public License
 // for more details.
 //
-// You should have received a copy of the GNU General 
-// Public License along with this program; if not, see 
-// <http://www.gnu.org/licenses/>. 
+// You should have received a copy of the GNU General
+// Public License along with this program; if not, see
+// <http://www.gnu.org/licenses/>.
 //
 #include "_template_.h"
 
@@ -27,7 +27,7 @@ namespace INCLUDE_GARDENER
 std::ostream& operator<<( std::ostream& os, const _Template_& tpl )
 {
    os  << "_Template_ ("  << reinterpret_cast< const void* >( &tpl ) << "): "
-       << "TODO=" << "Add more content of this instance here @ " 
+       << "TODO=" << "Add more content of this instance here @ "
        << __FILE__ << ":" << __LINE__;
    return os;
 }
